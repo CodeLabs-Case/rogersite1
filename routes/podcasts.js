27521caf2cd859fde1f1c7245c33eb5ce7f9e454
blackBoxMod <davis.gamer07@gmail.com>
2020-11-24 05:19:46 -0500

    Spelling fix

--- routes/podcasts.js
+++ routes/podcasts.js
@@ -2,10 +2,16 @@ const router = require('express').Router()
 const path = require('path')
 const fs = require('fs')
 
-router.route('/').get((req, res)=>{
+router.route('/').get((req, res, err)=>{
+    if(err){
+        throw err
+    }
+
     var content = fs.readFileSync('./database/collection0.json')
     var jsonContent = JSON.parse(content)
 
+
+    
     res.render(path.join('/var/app/current/views/podcasts.ejs'))
 })
 
