183839e20735b2ffb2602fd1ad3afee099b52cf6
blackBoxMod <davis.gamer07@gmail.com>
2020-11-30 10:25:50 -0500

    Inject database information into the views 10

--- routes/articles.js
+++ routes/articles.js
@@ -7,11 +7,11 @@ router.route('/').get((req, res, err)=>{
         console.log(err)
     }
 
-    // var content = fs.readFileSync('./database/collection0.json')
-    // var jsonContent = JSON.parse(content)
+    var content = fs.readFileSync('./database/collection1.json')
+    var jsonContent = JSON.parse(content)
 
     // Production
-    res.render(path.join('/var/app/current/views/articles.ejs'))
+    res.render(path.join('/var/app/current/views/articles.ejs'), {data: jsonContent})
     // Development
     // res.render(path.join('C:/Users/davis/OneDrive/Documents/Development/Freelance/rogersite1/routes/articles.js'))
     
