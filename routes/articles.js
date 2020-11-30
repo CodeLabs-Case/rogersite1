848aec2e9ea5fdc37a884a44698ee37555f52d22
blackBoxMod <davis.gamer07@gmail.com>
2020-11-30 11:33:24 -0500

    Inject database information into the views 25

--- routes/articles.js
+++ routes/articles.js
@@ -2,6 +2,8 @@ const router = require('express').Router()
 const path = require('path')
 const fs = require('fs')
 
+
+
 router.route('/').get((req, res, err)=>{
     if(err){
         console.log(err)
