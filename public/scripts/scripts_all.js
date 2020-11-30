3174cae2d6655c07eba6f4898128762605321429
blackBoxMod <davis.gamer07@gmail.com>
2020-11-30 08:02:13 -0500

    Fixing body height orientation issue

--- public/scripts/scripts_all.js
+++ public/scripts/scripts_all.js
@@ -1,4 +1,15 @@
 $(document).ready(()=>{
     var windowHeight = $(window).height()
     $('.div-body').css('height', windowHeight)
+
+    // Check on rotation
+    $(window).on( "orientationchange", (event)=>{
+        if(!window.screen.orientation.angle){
+            //alert('Portrait')
+            location.reload();
+        } else {
+            //alert('Landscape')
+            location.reload();
+        }
+    })
 })
