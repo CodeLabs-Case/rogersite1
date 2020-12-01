5c6127f688464b080902b31a89926a8f8ece8d54
blackBoxMod <davis.gamer07@gmail.com>
2020-11-30 20:14:29 -0500

    Building Control Panel 17

--- public/scripts/scripts_cp.js
+++ public/scripts/scripts_cp.js
@@ -1,15 +1,11 @@
 $(document).ready(()=>{
     $('.testp').on("click", ()=>{
-        alert("P Click")
-
-        $('.testp').css('z-index', 1)
-        $('.testa').css('z-index', 0)
+        $('.div-wrapper-p').css('z-index', "1")
+        $('.div-wrapper-a').css('z-index', "0")
     })
 
     $('.testa').on("click", ()=>{
-        alert("A Click")
-
-        $('.testp').css('z-index', 0)
-        $('.testa').css('z-index', 1)
+        $('.div-wrapper-p').css('z-index', "0")
+        $('.div-wrapper-a').css('z-index', "1")
     })
 })
