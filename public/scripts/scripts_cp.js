27a61987716f986e5bc6af083a676c1473adb9d9
blackBoxMod <davis.gamer07@gmail.com>
2020-11-30 20:46:24 -0500

    Building Control Panel 26

--- public/scripts/scripts_cp.js
+++ public/scripts/scripts_cp.js
@@ -5,7 +5,7 @@ $(document).ready(()=>{
         $('.div-wrapper-p').css('z-index', "1")
         $('.div-wrapper-a').css('z-index', "0")
 
-        $('.div-underline-move').animate({"justify-self": "start"}, 500)
+        $('.div-underline-move').animate({"justify-self": "140px"}, 500)
         // $('.div-underline-move').css("margin-left", "0px")
     })
 
@@ -13,7 +13,7 @@ $(document).ready(()=>{
         $('.div-wrapper-p').css('z-index', "0")
         $('.div-wrapper-a').css('z-index', "1")
 
-        $('.div-underline-move').animate({"justify-self": "end"}, 500)
+        $('.div-underline-move').animate({"margin-left": "140px"}, 500)
         // $('.div-underline-move').css("margin-right", "0px")
     })
 
