6c3b655f7476a72e4a2dac1f2560617e63dbe5e9
blackBoxMod <davis.gamer07@gmail.com>
2020-11-30 21:40:39 -0500

    Redesigning home 5

--- public/scripts/scripts_home.js
+++ public/scripts/scripts_home.js
@@ -1,4 +1,18 @@
 $(document).ready(()=>{
+    var windowHeight = $(window).height()
+    $('.div-body').css('height', windowHeight)
+
+    // Check on rotation
+    $(window).on( "orientationchange", (event)=>{
+        if(!window.screen.orientation.angle){
+            //alert('Portrait')
+            location.reload();
+        } else {
+            //alert('Landscape')
+            location.reload();
+        }
+    })
+    
     $('.p-podcasts').on("click", ()=>{
         $('.div-wrapper-p').css('z-index', "1")
         $('.div-wrapper-a').css('z-index', "0")
