3f84c1203101705cf3cfaae42dbcfc373b755328
blackBoxMod <davis.gamer07@gmail.com>
2020-11-30 20:24:03 -0500

    Building Control Panel 20

--- public/scripts/scripts_cp.js
+++ public/scripts/scripts_cp.js
@@ -1,11 +1,29 @@
 $(document).ready(()=>{
-    $('.testp').on("click", ()=>{
+
+    $('.div-underline-move').animate({"margin-right": "140px"}, 500)
+    $('.div-underline-move').animate({"margin-left": "140px"}, 500)
+
+
+    $('.p-podcasts').on("click", ()=>{
         $('.div-wrapper-p').css('z-index', "1")
         $('.div-wrapper-a').css('z-index', "0")
     })
 
-    $('.testa').on("click", ()=>{
+    $('.p-articles').on("click", ()=>{
         $('.div-wrapper-p').css('z-index', "0")
         $('.div-wrapper-a').css('z-index', "1")
+    })
+
+
+
+    // Check on rotation
+    $(window).on( "orientationchange", (event)=>{
+        if(!window.screen.orientation.angle){
+            //alert('Portrait')
+            location.reload();
+        } else {
+            //alert('Landscape')
+            location.reload();
+        }
     })
 })
