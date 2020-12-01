f963afdf7ff22a35473a020d2bea917c37046a4f
blackBoxMod <davis.gamer07@gmail.com>
2020-11-30 19:12:53 -0500

    Building Control Panel 2

--- routes/cp.js
+++ routes/cp.js
@@ -7,7 +7,21 @@ router.route('/').get((req, res, err) => {
     if(err){
         console.log(err)
     }
-    res.render(path.join('/var/app/current/views/cp.ejs'))
+
+    var podcasts = fs.readFileSync('/var/app/current/database/collection0.json')
+    var jsonPodcasts = JSON.parse(podcasts)
+
+
+    var articles = fs.readFileSync('/var/app/current/database/collection1.json')
+    var jsonArticles = JSON.parse(articles)
+
+
+
+
+    res.render(path.join('/var/app/current/views/cp.ejs'), { 
+        podcasts: jsonPodcasts,
+        articles: jsonArticles
+    })
 })
 
 
