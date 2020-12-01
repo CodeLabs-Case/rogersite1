2c03644b9b68981260e58e1334d7d51b0751f31f
blackBoxMod <davis.gamer07@gmail.com>
2020-11-30 19:33:12 -0500

    Building Control Panel 7

--- routes/cp.js
+++ routes/cp.js
@@ -8,17 +8,36 @@ router.route('/').get((req, res, err) => {
         console.log(err)
     }
 
-    var podcasts = fs.readFileSync('/var/app/current/database/collection0.json')
-    var jsonPodcasts = JSON.parse(podcasts)
+    // Pull the data from the forms
+    // Try two ways to get the data from the forms for testing purposes
+    // const { email, password } = req.body
+
+    var email = req.body.email
+    var password = req.body.password
+
+    // Read in the database file
+    var content = fs.readFileSync('/var/app/current/database/signin.json')
+    var jsonContent = JSON.parse(content)
 
 
-    // var article = fs.readFileSync('/var/app/current/database/collection1.json')
-    // var jsonArticles = JSON.parse(article)
+    // Compare the data
+    if(jsonContent.email == email && jsonContent.password == password){
 
+        // Get the data for the page
+        var podcasts = fs.readFileSync('/var/app/current/database/collection0.json')
+        var jsonPodcasts = JSON.parse(podcasts)
 
+        // var article = fs.readFileSync('/var/app/current/database/collection1.json')
+        // var jsonArticles = JSON.parse(article)
 
+        // Send the page
+        res.render(path.join('/var/app/current/views/cp.ejs'), {data: jsonPodcasts})
 
-    res.render(path.join('/var/app/current/views/cp.ejs'), {data: jsonPodcasts})
+    } else {
+
+        res.render(path.join('/var/app/current/views/admin.ejs'))
+        
+    }    
 })
 
 
