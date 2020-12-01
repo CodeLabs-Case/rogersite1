58d9e2a4e826c122879e00388945a5651b69e6c3
blackBoxMod <davis.gamer07@gmail.com>
2020-11-30 20:42:30 -0500

    Building Control Panel 25

--- public/scripts/scripts_cp.js
+++ public/scripts/scripts_cp.js
@@ -1,17 +1,20 @@
 $(document).ready(()=>{
 
-    $('.div-underline-move').animate({"margin-right": "140px"}, 500)
-    $('.div-underline-move').animate({"margin-left": "140px"}, 500)
-
 
     $('.p-podcasts').on("click", ()=>{
         $('.div-wrapper-p').css('z-index', "1")
         $('.div-wrapper-a').css('z-index', "0")
+
+        $('.div-underline-move').animate({"margin-right": "140px"}, 500)
+        $('.div-underline-move').css("margin-left", "0px")
     })
 
     $('.p-articles').on("click", ()=>{
         $('.div-wrapper-p').css('z-index', "0")
         $('.div-wrapper-a').css('z-index', "1")
+
+        $('.div-underline-move').animate({"margin-left": "140px"}, 500)
+        $('.div-underline-move').css("margin-right", "0px")
     })
 
 
