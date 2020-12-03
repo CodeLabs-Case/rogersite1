a645892d5e7cae2a47c9a7f42fd8be0605a6ac4e
blackBoxMod <davis.gamer07@gmail.com>
2020-12-03 14:33:48 -0500

    Implementing add functionality 10

--- public/scripts/scripts_cp.js
+++ public/scripts/scripts_cp.js
@@ -5,12 +5,18 @@ $(document).ready(()=>{
         $('.div-wrapper-p').css('z-index', "1")
         $('.div-wrapper-a').css('z-index', "0")
 
+        $('.div-wrapper-p').css('opacity', "1")
+        $('.div-wrapper-a').css('opacity', "0")
+
         $('.div-underline-move').animate({"margin-left": "0px"}, 500)
     })
 
     $('.p-articles').on("click", ()=>{
         $('.div-wrapper-p').css('z-index', "0")
         $('.div-wrapper-a').css('z-index', "1")
+
+        $('.div-wrapper-p').css("opacity", "0")
+        $('.div-wrapper-a').css('opacity', "1")
         
         $('.div-underline-move').animate({"margin-left": "140px"}, 500)
     })
