356f4dd5e35b5d62767652d9a6eb824e38a90fdd
blackBoxMod <davis.gamer07@gmail.com>
2020-12-06 11:35:29 -0500

    Implement SESSION for login 20

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ const app = express()
 
 app.set('viewengine', 'ejs')
 
-// app.use(flash())
+app.use(flash())
 // app.use(session({
 //     secret: process.env.SESSION_SECRET,
 //     resave: false,
