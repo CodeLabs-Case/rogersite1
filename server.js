4319195d620fde9ec62d3775b117c453ed4fad9e
blackBoxMod <davis.gamer07@gmail.com>
2020-12-06 12:37:11 -0500

    Implement SESSION for login 38

--- server.js
+++ server.js
@@ -23,12 +23,12 @@ const users = [
 ]
 
 // This is for setting up the login functionality
-const initializePassport = require('./passport-config')
-initializePassport(
-    passport,
-    email => users.find(user => user.email === email),
-    id => users.find(user => user.id === id)
-)
+// const initializePassport = require('./passport-config')
+// initializePassport(
+//     passport,
+//     email => users.find(user => user.email === email),
+//     id => users.find(user => user.id === id)
+// )
 
 // const initializePassport = require('./passport-config')
 // initializePassport(
