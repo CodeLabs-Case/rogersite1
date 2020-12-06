99554c757b9edf50859df3d614b3ce0df796e630
blackBoxMod <davis.gamer07@gmail.com>
2020-12-06 17:48:10 -0500

    Implement SESSION for login 50

--- routes/cp.js
+++ routes/cp.js
@@ -9,39 +9,23 @@ router.get('/', (req, res, err) => {
         console.log(err)
     }
 
-    // // Pull the data from the forms
-    // // Try two ways to get the data from the forms for testing purposes
-    // // const { email, password } = req.body
+    // Pull the data from the forms
+    // Try two ways to get the data from the forms for testing purposes
+    // const { email, password } = req.body
 
-    // var email = req.body.email
-    // var password = req.body.password
+    // Read in the database file
+    var content = fs.readFileSync('/var/app/current/database/signin.json')
+    var jsonContent = JSON.parse(content)
 
-    // // Read in the database file
-    // var content = fs.readFileSync('/var/app/current/database/signin.json')
-    // var jsonContent = JSON.parse(content)
+    // Get the data for the page
+    var podcasts = fs.readFileSync('/var/app/current/database/collection0.json')
+    var jsonPodcasts = JSON.parse(podcasts)
 
+    var articles = fs.readFileSync('/var/app/current/database/collection1.json')
+    var jsonArticles = JSON.parse(articles)
 
-    // // Compare the data
-    // if(jsonContent.email == email && jsonContent.password == password){
-
-    //     // Get the data for the page
-    //     var podcasts = fs.readFileSync('/var/app/current/database/collection0.json')
-    //     var jsonPodcasts = JSON.parse(podcasts)
-
-    //     var articles = fs.readFileSync('/var/app/current/database/collection1.json')
-    //     var jsonArticles = JSON.parse(articles)
-
-    //     // Send the page
-    //     res.render(path.join('/var/app/current/views/cp.ejs'), {p: jsonPodcasts, a: jsonArticles})
-
-    // } else {
-
-    //     res.render(path.join('/var/app/current/views/admin.ejs'))
-
-    // }
-
-
-    res.render(path.join('/var/app/current/views/cp.ejs'))
+    // Send the page
+    res.render(path.join('/var/app/current/views/cp.ejs'), {p: jsonPodcasts, a: jsonArticles})
 })
 router.route('/podcast').get((req, res, err) => {
     if(err){ 
