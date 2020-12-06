436676a9c81f1199da953ed86d66bc142b1a4d80
blackBoxMod <davis.gamer07@gmail.com>
2020-12-06 12:47:15 -0500

    Implement SESSION for login 44

--- server.js
+++ server.js
@@ -14,21 +14,21 @@ const session = require('express-session')
 const methodOverride = require('method-override')
 const app = express()
 
-// const users = [
-//     {
-//         "id": 1,
-//         "email": "roger@gmail.com",
-//         "password": "things"
-//     }
-// ]
+const users = [
+    {
+        "id": 1,
+        "email": "roger@gmail.com",
+        "password": "things"
+    }
+]
 
 // This is for setting up the login functionality
-// const initializePassport = require('./passport-config')
-// initializePassport(
-//     passport,
-//     email => users.find(user => user.email === email),
-//     id => users.find(user => user.id === id)
-// )
+const initializePassport = require('./passport-config')
+initializePassport(
+    passport,
+    email => users.find(user => user.email === email),
+    id => users.find(user => user.id === id)
+)
 
 // const initializePassport = require('./passport-config')
 // initializePassport(
