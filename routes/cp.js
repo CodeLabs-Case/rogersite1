2216cc35b0e281f21d5f3568e0bea403e29e08ed
blackBoxMod <davis.gamer07@gmail.com>
2020-12-08 19:45:58 -0500

    Fixing newline issue 7

--- routes/cp.js
+++ routes/cp.js
@@ -96,7 +96,8 @@ router.route('/addpodcast').post(checkAuthenticated, (req, res) => {
 })
 router.route('/addarticle').post(checkAuthenticated, (req, res) => {
 
-    var data = req.body
+    var title = req.body.title
+    var text = req.body.text
 
     // var titleString = JSON.stringify(title)
     // var bodyString = JSON.stringify(body)
@@ -113,8 +114,8 @@ router.route('/addarticle').post(checkAuthenticated, (req, res) => {
     var date = month + " - " + day + " - " + year
 
     var temp = {
-        "title": data.tite,
-        "body": data.body,
+        "title": title,
+        "body": text,
         "date": date
     }
 
