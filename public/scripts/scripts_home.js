bbe69b15c9a4e2e3eeeb395d03d92236cecff481
blackBoxMod <davis.gamer07@gmail.com>
2020-12-17 17:04:00 -0500

    Version 2. 135

--- public/scripts/scripts_home.js
+++ public/scripts/scripts_home.js
@@ -114,7 +114,7 @@ $(document).ready(()=>{
 
 
     // Make the proper podcast/article jump to the top of the list when the title in the sidebar is clicked on
-    $('.ul-titles li').click(()=>{
+    $('.ul-titles li').on('click', ()=>{
         var index = $('.ul-titles li').attr('class')
         alert(index)
     })
