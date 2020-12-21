bb2496a1627a7d72910304a9a0fa46f8e2a72eff
blackBoxMod <davis.gamer07@gmail.com>
2020-12-21 11:16:26 -0500

    Version 2. 333

--- public/scripts/scripts_home.js
+++ public/scripts/scripts_home.js
@@ -106,10 +106,10 @@ $(document).ready(()=>{
     // Make the image for the podcast become opaque and grow when hovered over ...
     // ... and then do the inverse
     $('.img-link').on('mouseenter', function(){
-        $(this).stop(true, true).animate({ "opacity": "1.0" }, 500)
+        $(this).stop(true, true).animate({ "opacity": "0.5" }, 500)
     })
     $('.img-link').on('mouseleave', function(){
-        $(this).stop(true, true).animate({ "opacity": "0.5" }, 500)
+        $(this).stop(true, true).animate({ "opacity": "1.0" }, 500)
     })
 
 
